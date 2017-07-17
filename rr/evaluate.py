e3e2528949a36fcedcb3b10334095da68fb41ece
maidousj <maidousj@163.com>
2017-07-17 15:25:20 +0800

    add reliability cruve function

--- rr/evaluate.py
+++ rr/evaluate.py
@@ -8,6 +8,10 @@ Date: 2017-7-14
 
 import csv
 from sklearn import metrics
+import matplotlib
+matplotlib.use('Agg')
+import matplotlib.pyplot as plt
+from sklearn.calibration import calibration_curve
 
 def loadFile(fileName):
     ret = []
@@ -18,7 +22,10 @@ def loadFile(fileName):
         for ind, row in enumerate(content):
             if ind%100000 == 0:
                 print ("load {} {} rows".format(fileName, ind))
-            ret.append(int(row[2]))
+            if len(row[2]) == 1:
+                ret.append(int(row[2]))
+            else:
+                ret.append(float(row[2]))
 
     return ret
 
@@ -29,10 +36,38 @@ def evaluate(actual, predictFileName):
     print ("recall_score:\n for {} = {}".format(predictFileName, metrics.recall_score(actual, predict)))
     print ("f1_score:\n for {} = {}".format(predictFileName, metrics.f1_score(actual, predict)))
 
+def reliabilityCruve(actual, predictions, predictionsFlip, picName):
+    fig = plt.figure(1, figsize=(10, 10))
+    ax1 = plt.subplot2grid((3, 1), (0, 0), rowspan=2)
+
+    clf_score = metrics.brier_score_loss(actual, predictions, pos_label=1)
+    clf_score_flip = metrics.brier_score_loss(actual, predictionsFlip, pos_label=1)
+
+    fraction_of_positives, mean_predicted_value = calibration_curve(actual, predictions, n_bins=10)
+    fraction_of_positives_flip, mean_predicted_value_flip = calibration_curve(actual, predictionsFlip, n_bins=10)
+
+    ax1.plot(mean_predicted_value, fraction_of_positives, "s-", label="clf_score (%1.3f)" % (clf_score))
+    ax1.plot(mean_predicted_value_flip, fraction_of_positives_flip, "r-", label="clf_score (%1.3f)" % (clf_score_flip))
+
+    ax1.set_ylabel("Fraction of positives")
+    ax1.set_ylim([-0.05, 1.05])
+    ax1.legend(loc="lower right")
+    ax1.set_title('Calibration plots  (reliability curve)')
+    plt.savefig(picName)
+
 if __name__ == '__main__':
     actFileName = "/data/pythonsolution/trunoutput/test.csv"
-    preFileName = "/data/pythonsolution/trunoutput/new_test.csv"
-    preFlipFileName = "/data/pythonsolution/trunoutput/new_test_flip.csv"
+#    preFileName = "/data/pythonsolution/trunoutput/new_test.csv"
+#    preFlipFileName = "/data/pythonsolution/trunoutput/new_test_flip.csv"
+    preFileName = "/data/pythonsolution/trunoutput/sub_proba_test.csv"
+    preFlipFileName = "/data/pythonsolution/trunoutput/sub_proba_test_flip_0.5.csv"
+
     actual = loadFile(actFileName)
-    evaluate(actual, preFileName)
-    evaluate(actual, preFlipFileName)
+    predictions = loadFile(preFileName)
+    predictionsFlip = loadFile(preFlipFileName)
+
+#    evaluate(actual, preFileName)
+#    evaluate(actual, preFlipFileName)
+
+    reliabilityCruve(actual, predictions, predictionsFlip, "relCruve.png")
+
